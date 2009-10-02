6bb101e1e6c893111d6a8c0cb57042046a8abd26
Conal Elliott <conal@conal.net>
2009-10-02 13:11:24 -0700

    fixed version skew
    darcs-hash:20091002201124-fb517-3e21c4b355d0120367bf9a00a11910f094adb1bc.gz

--- src/Data/DDeepArrow.hs
+++ src/Data/DDeepArrow.hs
@@ -21,6 +21,11 @@ module Data.DDeepArrow
     DArrow(..), DVal(..)
   ) where
 
+#if __GLASGOW_HASKELL__ >= 609
+import Control.Category
+import Prelude hiding ((.), id)
+#endif
+
 import Control.Arrow
 #if __GLASGOW_HASKELL__ < 610
                       hiding (pure)
@@ -61,9 +66,17 @@ data DArrow :: * -> * -> * where
  SndA     :: (a,b) `DArrow` b
  SwapA    :: (a,b) `DArrow` (b,a)
 
+#if __GLASGOW_HASKELL__ >= 609
+instance Category DArrow where
+  id  = IdA
+  (.) = flip Compose
+#endif
+
 instance Arrow DArrow where
   arr      = error "no arr/pure for DDeepArrow"
+#if __GLASGOW_HASKELL__ < 609
   (>>>)    = Compose
+#endif
   first    = First
   second   = Second
 
